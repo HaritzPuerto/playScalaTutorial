1af718e919b9f509715388e1db483271e7dbccbe
Haritz Puerto <haritzpuerto94@gmail.com>
2015-08-04 23:19:16 +0200

    Sign Up finished

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,18 +1,43 @@
 package controllers
 
+import javax.inject.Inject
+
 import models.User
 import play.api._
+import play.api.data.Form
+import play.api.data.Forms.{mapping, nonEmptyText}
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
 
-class Application extends Controller {
+class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index(userForm))
   }
 
   def list = Action { implicit request =>
     val users = User.findAll
     Ok(views.html.users.list(users))
+  }
+
+  private val userForm: Form[User] = Form(
+    mapping(
+      "username" -> nonEmptyText, //fields of the formulary.
+      "password" -> nonEmptyText //nonEmptyText forces the field to not to be empty
+    )(User.apply)(User.unapply)
+  )
+
+  def signUp = Action { implicit request =>
+    val newUser = userForm.bindFromRequest()
+    newUser.fold(
+      hasErrors = { form =>
+        Redirect(routes.Application.index())
+      },
+      success = { user =>
+        User.signUp(user)
+        Redirect(routes.Application.list())
+      }
+    )
   }
 
 }
--- app/models/User.scala
+++ app/models/User.scala
@@ -11,6 +11,18 @@ object User {
     User("Josh", "Josh"),
     User("Dave", "Dave")
   )
+
+  /**
+   * Retrieve all users in the users variable
+   * @return
+   */
   def findAll = users.toList
 
+  /**
+   * Add user to the users variable.
+   * @param user
+   */
+  def signUp(user: User): Unit = {
+    users = users + user
+  }
 }
